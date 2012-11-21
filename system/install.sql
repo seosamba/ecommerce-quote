954b48c5b4f6f9061256d0b8892ab713a37577c7
Eugene Nezhuta <theneiam@gmail.com>
2012-11-21 16:10:56 +0200

    Quote system small fix. Installation improvements

--- system/install.sql
+++ system/install.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `shopping_quote` (
   `status` enum('new','sent','sold','lost') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'new',
   `cart_id` int(10) unsigned DEFAULT NULL,
   `edited_by` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `valid_until` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
   `user_id` int(10) unsigned DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -16,9 +16,9 @@ CREATE TABLE IF NOT EXISTS `shopping_quote` (
   KEY `cart_id` (`cart_id`),
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 ALTER TABLE `shopping_quote`
   ADD CONSTRAINT `shopping_quote_ibfk_2` FOREIGN KEY (`cart_id`) REFERENCES `shopping_cart_session` (`id`) ON DELETE SET NULL,
   ADD CONSTRAINT `shopping_quote_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION;
-
 UPDATE `plugin` SET `tags`='ecommerce' WHERE `name` = 'quote';
+INSERT INTO `email_triggers` (`enabled`, `trigger_name`, `observer`) VALUES( '1', 'quote_newquote', 'Quote_Tools_QuoteMailWatchdog');
+INSERT INTO `template_type` (`id`, `title`) VALUES ('typequote', 'Quote');
--- system/uninstall.sql
+++ system/uninstall.sql
@@ -1,1 +1,3 @@
 DROP TABLE IF EXISTS `shopping_quote`;
+DELETE FROM `email_triggers` WHERE `trigger_name` = 'quote_newquote';
+DELETE FROM `template_type` WHERE `id` = 'typequote';
