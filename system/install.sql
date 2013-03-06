82bba03fd21d22673aaf6de8778f4f0f0e6a1be7
Eugene NE Nezhuta <theneiam@gmail.com>
2013-03-06 18:16:26 +0200

    Installer updated

--- system/install.sql
+++ system/install.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS `shopping_quote`;
-CREATE TABLE IF NOT EXISTS `shopping_quote` (
+CREATE TABLE `shopping_quote` (
   `id` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `status` enum('new','sent','sold','lost') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'new',
+  `disclaimer` text COLLATE utf8_unicode_ci,
   `cart_id` int(10) unsigned DEFAULT NULL,
   `edited_by` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `expires_at` timestamp NULL DEFAULT NULL,
@@ -14,12 +15,12 @@ CREATE TABLE IF NOT EXISTS `shopping_quote` (
   KEY `status` (`status`),
   KEY `edited_by` (`edited_by`),
   KEY `cart_id` (`cart_id`),
-  KEY `user_id` (`user_id`)
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `shopping_quote_ibfk_2` FOREIGN KEY (`cart_id`) REFERENCES `shopping_cart_session` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `shopping_quote_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-ALTER TABLE `shopping_quote`
-  ADD CONSTRAINT `shopping_quote_ibfk_2` FOREIGN KEY (`cart_id`) REFERENCES `shopping_cart_session` (`id`) ON DELETE SET NULL,
-  ADD CONSTRAINT `shopping_quote_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION;
 UPDATE `plugin` SET `tags`='ecommerce' WHERE `name` = 'quote';
 INSERT INTO `email_triggers` (`enabled`, `trigger_name`, `observer`) VALUES( '1', 'quote_created', 'Quote_Tools_QuoteMailWatchdog');
 INSERT INTO `email_triggers` (`enabled`, `trigger_name`, `observer`) VALUES( '1', 'quote_updated', 'Quote_Tools_QuoteMailWatchdog');
 INSERT INTO `template_type` (`id`, `title`) VALUES ('typequote', 'Quote');
+INSERT INTO `page_option` (`id`, `title`, `context`, `active`) VALUES ('option_quotepage', 'Quote page', 'Quote system', 1);
