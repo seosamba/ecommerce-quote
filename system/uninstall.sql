ec1964738aed5e33c1a9ba3ebc5090756a4e1438
Eugene NE Nezhuta <theneiam@gmail.com>
2013-03-12 13:02:49 +0200

    Uninstall updated

--- system/uninstall.sql
+++ system/uninstall.sql
@@ -2,3 +2,4 @@ DROP TABLE IF EXISTS `shopping_quote`;
 DELETE FROM `email_triggers` WHERE `trigger_name` = 'quote_created';
 DELETE FROM `email_triggers` WHERE `trigger_name` = 'quote_updated';
 DELETE FROM `template_type` WHERE `id` = 'typequote';
+DELETE FROM `page_option` WHERE `id` = 'option_quotepage';
