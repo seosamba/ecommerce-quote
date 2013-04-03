239d12f3989c7b85874359d31ec8b63be0c25154
Eugene NE Nezhuta <theneiam@gmail.com>
2013-04-03 19:03:27 +0300

    Major quote system update

--- web/js/misc/controls.js
+++ web/js/misc/controls.js
@@ -1,114 +1,70 @@
+// Quote system. Controll panel.
+
 $(function() {
+    //apply jQueryUI buttons to quote controls
     $('.quote-control').button();
 
-    $(document).on('click', '.save-action', function(e) { //quote controls handling
-        var control     = $(e.currentTarget);
-        var sendMail    = control.data('sendmail');
-        if(parseInt(sendMail)) {
+    // current quote id
+    var quoteId = $('#quote-id').val();
+
+    // quote control click handling
+    $(document).on('click', '.quote-control', function(e) {
+        var control  = $(e.currentTarget);
+        if(parseInt(control.data('sendmail')) == 1) {
             showMailMessageEdit(control.data('trigger'), function(message) {
-                updateQuote(control.data('qid'), sendMail, message);
+                updateQuote(quoteId, true, message);
             });
         } else {
-            updateQuote(control.data('qid'), sendMail);
+            updateQuote(quoteId, false);
         }
-    }).on('click', '#same-for-shipping', function(e) {  // billing form "same for shipping" checkbox handling
-        var shippingForm = $('#shipping-user-address');
-        var billingForm  = $('#plugin-quote-quoteform');
-        var check        = $(e.currentTarget);
-        if(shippingForm.length) {
-            $(':input[name], select[name]', billingForm).each(function() {
-                var shippingFormEl = $('[name=' + $(this).attr('name') + ']', shippingForm);
-                if(check.prop('checked')) {
-                    if($(this).hasClass('state') && $(this).is(':visible')) {
-                        $('select.state', shippingForm).html($(this).html()).parent('div').show();
-                    }
-                    shippingFormEl.val($(this).val()).attr('readonly', true);
-                } else {
-                    shippingFormEl.val('').removeAttr('readonly');
-                    $('select.state', shippingForm).parent('div').hide();
-                }
-            });
-        }
-    }).on('blur', '.quote-qty', function(e) { // product quantity control handling
-        var qtyControl = $(e.currentTarget);
-        var data       = {
-            qty : qtyControl.val(),
-            qid : $('.save-action:first').data('qid')
-        };
-        $.ajax({
-            url        : $('#website_url').val() + 'api/quote/products/id/' + qtyControl.attr('id') + '/type/qty/',
-            type       : 'put',
-            dataType   : 'json',
-            data       : JSON.stringify(data),
-            beforeSend : showSpinner
-        }).done(function(response) {
-                hideSpinner();
-                var itemTotalPrice   = qtyControl.closest('tr').find('span.price-total').text();
-                var totalPriceValue  = itemTotalPrice.replace(/[^\d]*/, '');
-                var singlePriceValue = qtyControl.closest('tr').find('span.price-unit').text().replace(/[^\d]*/, '');
-                var totalPriceRecounted = parseFloat(singlePriceValue) * data.qty;
-                qtyControl.closest('tr').find('span.price-total').text(itemTotalPrice.replace(totalPriceValue, totalPriceRecounted.toFixed(2)));
+    });
 
-                updateTotal(response);
-        });
-    }).on('click', '.remove-product', function(event) {
-        smoke.confirm('You are about to remove an item. Are you sure?', function(e) {
-            if(e) {
-                $.ajax({
-                    url      : $('#website_url').val() + 'api/quote/products/id/' + $(event.currentTarget).data('pid'),
-                    type     : 'delete',
-                    data     : JSON.stringify({qid: $('.save-action:first').data('qid')}),
-                    dataType : 'json',
-                    beforeSend: showSpinner
-                }).done(function(response) {
-                    $(event.currentTarget).closest('tr').remove();
+    // editable (qantity, price) fields handling
+    $(document).on('blur', '.quote-recalculate', function(e) {
+        var field = $(e.currentTarget);
+        var scope = field.data('scope');
+        var type  = field.data('type');
+
+        var data = {
+            qid   : quoteId,
+            type  : type,
+            value : field.val()
+        };
+
+        switch (scope) {
+            case 'quote-item':
+                var productId = field.data('pid');
+                var request   = _update('api/quote/products/id/' + productId, data)
+                request.done(function(response) {
                     hideSpinner();
-                    updateTotal(response);
+                    $.extend(data, {
+                        calculateProduct : true,
+                        productId        : productId,
+                        summary          : response
+                    });
+                    recalculate(data);
                 });
-            } else {
-                $('.smoke-base').remove();
-            }
-        }, {classname:"errors", 'ok':'Yes', 'cancel':'No'});
-    }).on('blur', '#quote-shipping-price', function() {
-        var quoteId = $('.save-action:first').data('qid');
-        var shippingPrice = parseFloat($(this).val());
-        if(!shippingPrice) {
-            $(this).val(0);
-            shippingPrice = 0;
+            break;
+            case 'quote-partial':
+                var request = _update('api/quote/quotes/', data);
+                request.done(function(response) {
+                    hideSpinner();
+                    $.extend(data, {summary:response});
+                    recalculate(data);
+                });
+            break;
         }
-        $.ajax({
-            url      : $('#website_url').val() + 'api/quote/quotes/',
-            type     : 'put',
-            data     : JSON.stringify({
-                id: quoteId,
-                partial: 'shipping',
-                shippingPrice: shippingPrice
-            }),
-            dataType : 'json'
-        }).done(function(response) {
-           $('.grand-total').text(response.grandTotalCurrency);
-        });
+    })
+
+    $(document).on('change', '#quote-discount-rate', function(e) {
+
     });
 });
 
 
-function updateTotal(options) {
-    $('.sub-total').text(options.subTotal);
-    $('.tax-total').text(options.totalTax);
-    $('.grand-total').text(options.total);
-}
-
-function updateQuote(quoteId, sendMail, mailMessage) {
-    if(typeof sendMail == 'undefined') {
-        sendMail = false;
-    }
-
-    if(typeof mailMessage == 'undefined') {
-        mailMessage = '';
-    }
-
+var updateQuote = function(quoteId, sendMail, mailMessage) {
     var data = {
-        id          : quoteId,
+        qid          : quoteId,
         sendMail    : sendMail,
         title       : $('#quote-title').val(),
         disclaimer  : $('.quote-disclaimer-text').val(),
@@ -116,17 +72,44 @@ function updateQuote(quoteId, sendMail, mailMessage) {
         expiresAt   : $('#datepicker-expires').val(),
         shipping    : $('#shipping-user-address').serialize(),
         billing     : $('#plugin-quote-quoteform').serialize(),
-        mailMessage : mailMessage
+        mailMessage : (sendMail) ? mailMessage : ''
     };
-    $.ajax({
-        url        : $('#website_url').val() + 'api/quote/quotes/',
+
+    var request = _update('api/quote/quotes/', data);
+    request.done(function(response, status, xhr) {
+        hideSpinner();
+    });
+}
+
+var _update = function(apiUrl, data) {
+    return $.ajax({
         type       : 'put',
+        url        : $('#website_url').val() + apiUrl,
         dataType   : 'json',
         data       : JSON.stringify(data),
         beforeSend : showSpinner
-    }).done(function(response) {
-            hideSpinner();
-            updateTotal(response);
-            showMessage('Quote information updated.');
     });
 }
+
+var recalculate = function(options) {
+    var symbol = $('#quote-currency').val();
+    if(options.hasOwnProperty('calculateProduct') && options.calculateProduct === true) {
+        var unitPriceContainer = $('input.price-unit[data-pid]="' + options.productId + '"');
+
+        var unitPrice  = parseFloat(unitPriceContainer.val());
+        var qty        = parseInt($('input.qty-unit[data-pid]="' + options.productId + '"').val())
+        var totalPrice = unitPrice * qty;
+
+        $('.price-total[data-pid]="' + options.productId + '"').text(symbol + totalPrice.toFixed(2));
+        unitPriceContainer.val(parseFloat(unitPrice).toFixed(2));
+    }
+    var summary = options.summary;
+
+    $('.sub-total').text(symbol + summary.subTotal.toFixed(2));
+    $('.tax-total').text(symbol + summary.totalTax.toFixed(2));
+    $('#quote-shipping-price').val(parseFloat(summary.shipping).toFixed(2));
+    $('#quote-discount').val(parseFloat(summary.discount).toFixed(2));
+    $('.grand-total').text(symbol + summary.total.toFixed(2));
+}
+
+
