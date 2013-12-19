49e1ca6343e05ffac8a5e75cc846f3517ef1a712
zhenya111 <zhenyaaynehz@gmail.com>
2013-12-19 15:52:30 +0200

    Added column "Owner" for sort quotes. Widget: {$quote:grid}. Added column "creator_id" to install.sql.

--- system/install.sql
+++ system/install.sql
@@ -8,6 +8,7 @@ CREATE TABLE `shopping_quote` (
   `delivery_type` tinytext COLLATE utf8_unicode_ci,
   `cart_id` int(10) unsigned DEFAULT NULL,
   `edited_by` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `creator_id` int(10) unsigned DEFAULT '0',
   `expires_at` timestamp NULL DEFAULT NULL,
   `user_id` int(10) unsigned DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
