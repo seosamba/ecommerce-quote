ed5fb0c6860dc523f0e9f38a5b85ebb61f4c3876
andreybudchenko <andrei@seosamba.com>
2014-01-08 17:56:36 +0200

    added quoteexpired magicspace

---
+++ system/app/MagicSpaces/Quoteexpired/Quoteexpired.php
@@ -0,0 +1,48 @@
+<?php
+
+
+class MagicSpaces_Quoteexpired_Quoteexpired extends Tools_MagicSpaces_Abstract {
+
+    private $_expired = true;
+    private $_quoteLost  = false;
+
+	protected function _run() {
+       $this->_parseParams();
+       $pageHelper  = Zend_Controller_Action_HelperBroker::getStaticHelper('page');
+       $quote       = Quote_Models_Mapper_QuoteMapper::getInstance()->find($pageHelper->clean($this->_toasterData['url']));
+       if($quote instanceof Quote_Models_Model_Quote){
+           if($quote->getStatus() === Quote_Models_Model_Quote::STATUS_LOST){
+               $this->_quoteLost = true;
+           }
+           if($this->_expired && $this->_quoteLost){
+               return $this->_spaceContent;
+           }elseif(!$this->_expired && !$this->_quoteLost){
+               return $this->_spaceContent;
+           }
+           return '';
+       }
+       return '';
+
+    }
+
+    /**
+     * Parse magic space parameters $_params
+     *
+     */
+    private function _parseParams() {
+        if (is_array($this->_params)) {
+            foreach ($this->_params as $key => $param) {
+                if (empty($param)) {
+                    continue;
+                } else {
+                    if ($param === 'not') {
+                        $this->_expired = false;
+                        continue;
+                    }
+                }
+            }
+        }
+
+    }
+
+}
