179d7528ba4dccf4c5945216d48305d9a6030c65
andreybudchenko <andrei@seosamba.com>
2014-03-05 12:52:45 +0200

    added more french translation

---
+++ web/js/nls/en_ln.js
@@ -0,0 +1,16 @@
+define({
+     "root": {
+        'You are about to remove a quote! Are you sure?' : 'You are about to remove a quote! Are you sure?',
+        'has been removed.'    : 'has been removed.',
+        'Quote'                : 'Quote',
+        'Try again later.'     : 'Try again later.',
+        'Can not remove quote' : 'Can not remove quote',
+        'status. Try again later.' : 'statut. Réessayez plus tard.',
+        'Can not update quote'     : 'Can not update quote',
+        'New quote'                : 'New quote',
+        'has been generated.'      : 'has been generated.',
+        'Your are about to remove a bunch of quotes! Are you sure?' : 'Your are about to remove a bunch of quotes! Are you sure?',
+        'You should pick at least one item!' : 'You should pick at least one item!'
+
+    }
+});
