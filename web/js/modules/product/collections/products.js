b9e0a7e2f4e75dc454ce72a0e283bf169b487b88
Pavel Kovalyov <sxe.funky@gmail.com>
2014-05-14 18:06:24 +0300

    Update quote product search to use organic search

--- web/js/modules/product/collections/products.js
+++ web/js/modules/product/collections/products.js
@@ -3,27 +3,32 @@ define([
     'backbone',
     '../models/product',
     'backbone.paginator'
-], function(_, Backbone, ProductModel){
+], function (_, Backbone, ProductModel) {
 
     var productsCollection = Backbone.Paginator.requestPager.extend({
         model: ProductModel,
         paginator_core: {
             dataType: 'json',
-            url:  $('#website_url').val() + 'api/store/products/'
+            url: $('#website_url').val() + 'api/store/products/'
         },
         paginator_ui: {
-            firstPage:    0,
-            currentPage:  0,
-            perPage:    100,
-            totalPages:  10
+            firstPage: 0,
+            currentPage: 0,
+            perPage: 100,
+            totalPages: 10
         },
         server_api: {
+            os: 1,
             count: true,
-            limit: function() { return this.perPage; },
-            offset: function() { return this.currentPage * this.perPage }
+            limit: function () {
+                return this.perPage;
+            },
+            offset: function () {
+                return this.currentPage * this.perPage;
+            }
         },
-        parse: function(response){
-            if (this.server_api.count){
+        parse: function (response) {
+            if (this.server_api.count) {
                 this.totalRecords = response.totalRecords;
             } else {
                 this.totalRecords = response.length;
@@ -31,10 +36,10 @@ define([
             this.totalPages = Math.floor(this.totalRecords / this.perPage);
             return this.server_api.count ? response.data : response;
         },
-        batch: function(method, data, options) {
+        batch: function (method, data, options) {
             var checked = this.where({checked: true});
-            var url     = $('#website_url').val() + 'api/quote/products/';
-            var ids     = _.pluck(checked, 'id').join(',');
+            var url = $('#website_url').val() + 'api/quote/products/';
+            var ids = _.pluck(checked, 'id').join(',');
 
             url += 'id/' + ids + '/';
 
@@ -44,11 +49,11 @@ define([
                 dataType: 'json',
                 beforeSend: showSpinner(),
                 data: JSON.stringify(data)
-            }).done(function() {
+            }).done(function () {
                 appView.products.pager();
-                if(typeof options != 'undefined' && (options instanceof Object)) {
-                    if(options.hasOwnProperty('success')) {
-                       options.success();
+                if (typeof options != 'undefined' && (options instanceof Object)) {
+                    if (options.hasOwnProperty('success')) {
+                        options.success();
                     }
                 }
 
@@ -56,5 +61,5 @@ define([
         }
     });
 
-	return productsCollection;
+    return productsCollection;
 });
