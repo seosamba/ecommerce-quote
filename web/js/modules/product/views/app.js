cad8fa98ad3acb4c0b8b1f4d0828222c8e194a9b
Pavel Kovalyov <sxe.funky@gmail.com>
2014-05-15 17:12:30 +0300

    Improve product search on quote management screen

--- web/js/modules/product/views/app.js
+++ web/js/modules/product/views/app.js
@@ -3,60 +3,104 @@ define([
     'backbone',
     '../collections/products',
     '../views/product'
-], function(_, Backbone, ProductsCollection, ProductView){
+], function (_, Backbone, ProductsCollection, ProductView) {
 
-	var quoteListView = Backbone.View.extend({
-		el: $('#products-container'),
-		events: {
-			'keypress #search': 'searchAction',
+    var quoteListView = Backbone.View.extend({
+        el: $('#products-container'),
+        events: {
+            'keypress #search': 'searchAction',
             'click .add-products': 'addAction'
-		},
-		initialize: function() {
+        },
+        initialize: function () {
             this.products = new ProductsCollection();
             this.products.on('reset', this.render, this);
             this.products.fetch();
 
             //init autocomplete
-            $.getJSON($('#website_url').val() + 'plugin/shopping/run/searchindex', function(response){
-                $('#search').autocomplete({
-                    minLength: 2,
-                    source: response,
-                    select: function(event, ui){
-                        $('#search').val(ui.item.value).trigger('keypress', true);
-                    }
-                });
+            $.getJSON($('#website_url').val() + 'plugin/shopping/run/searchindex', function (response) {
+                $('#search')
+                    .data({source: response})
+                    .autocomplete({
+                        minLength: 2,
+                        source: function (req, resp) {
+                            var data = $(this.element).data('source'),
+                                list;
+                            list = _.filter(data, function (str) {
+                                var t,
+                                    term = req.term.split(/\s+/);
+                                for (t in term) {
+                                    if (term.hasOwnProperty(t)) {
+                                        if (str.toLowerCase().search(term[t].toLowerCase()) === -1) {
+                                            return false;
+                                        }
+                                    }
+                                }
+                                return true;
+                            });
+                            console.info(data, list);
+                            resp(list);
+                        },
+                        select: function (event, ui) {
+                            $('#search').val(ui.item.value).trigger('keypress', true);
+                        }
+                    });
             });
-		},
-        addAction: function(e) {
+        },
+        addAction: function (e) {
             var splitedUrl = window.location.href.split('/');
-            var quoteId    = splitedUrl[splitedUrl.length - 1];
-            this.products.batch('post', {qid: quoteId}, {success: function(response) {
+            var quoteId = splitedUrl[splitedUrl.length - 1];
+            this.products.batch('post', {qid: quoteId}, {success: function (response) {
                 hideSpinner();
                 showMessage('Products added to the quote. Refreshing the quote page...');
                 window.parent.location.reload();
             }});
         },
-		render: function(){
+        render: function () {
             this.$('#products').empty();
-			this.products.each(function(product){
-				var view = new ProductView({model: product});
-				$(view.render().el).appendTo('#products');
+            this.products.each(function (product) {
+                var view = new ProductView({model: product});
+                $(view.render().el).appendTo('#products');
             });
+            if (this.products.length === 1) {
+                this.renderRelatedFor(this.products.first());
+            }
 
             this.$('img.lazy').lazyload({
                 container: this.$('#products'),
                 effect: 'fadeIn'
             });
         },
-        searchAction: function(e, force) {
-            if(e.keyCode == 13 || force) {
-                this.products.server_api.key = function() {return e.currentTarget.value; }
+        renderRelatedFor: function (product) {
+            console.log(product.get('related'));
+            if (!_.isEmpty(product.get('related'))) {
+                var relateds = new ProductsCollection(),
+                    placeholder = $('<img class="mt50px" src="' + $('#website_url').val() + 'system/images/spinner2.gif">');
+
+                this.$('#products').append(placeholder);
+                relateds.server_api.count = false;
+                relateds.on('reset', function (collection) {
+                    placeholder.remove();
+                    collec  tion.each(function (product) {
+                        var view = new ProductView({model: product});
+                        $(view.render().el).appendTo('#products');
+                    });
+                }, this);
+                relateds.fetch({reset: true, data: {id: product.get('related').join(',')}});
+
+            }
+
+        },
+        searchAction: function (e, force) {
+            if (e.keyCode == 13 || force) {
+                this.products.server_api.key = function () {
+                    return e.currentTarget.value;
+                }
                 this.products.pager();
                 $(e.target).autocomplete('close');
             }
-		}
-	});
+        }
+    });
 
-	return quoteListView;
+    return quoteListView;
 
 });
