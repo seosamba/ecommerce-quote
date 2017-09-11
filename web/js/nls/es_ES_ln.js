74792b0f06403b38b57ffeeb09fb334aede6427c
Vorobyov Valentin <vorobyovvalentin@gmail.com>
2017-09-11 10:20:03 +0300

    Add spain translate

---
+++ web/js/nls/es_ES_ln.js
@@ -0,0 +1,16 @@
+define({
+    "root": {
+        'You are about to remove a quote! Are you sure?' : '¿Desea eliminar el pedido?',
+        'has been removed.'    : 'Eliminado.',
+        'Quote'                : 'Presupuesto',
+        'Try again later.'     : 'por favor inténtelo más tarde.',
+        'Can not remove quote.' : 'No se puede eliminar',
+        'status. Try again later.' : 'el presupuesto, por favor inténtelo más tarde.',
+        'Can not update quote'     : 'No se puede actualizar el presupuesto',
+        'New quote'                : 'nuevo presupuesto',
+        'has been generated.'      : 'Presupuesto generado',
+        'Your are about to remove a bunch of quotes! Are you sure?' : '¿Desea eliminar los presupuestos seleccionados?',
+        'You should pick at least one item!' : 'Debe seleccionar al menos un producto'
+
+    }
+});
