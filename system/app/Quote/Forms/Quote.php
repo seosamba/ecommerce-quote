04cc5d6de39efc802ce8348934cb39be541be1f1
Valentin Vorobyov <vorobyovvalentin@gmail.com>
2018-08-08 09:31:26 +0300

    Add prefix into leftColumn block

--- system/app/Quote/Forms/Quote.php
+++ system/app/Quote/Forms/Quote.php
@@ -58,7 +58,7 @@ class Quote_Forms_Quote extends Forms_Address_Abstract {
 
         //adding display groups
         $this->addDisplayGroups(array(
-			'leftColumn'  => array('firstname', 'lastname', 'company', 'email', 'address1', 'address2'),
+			'leftColumn'  => array('prefix', 'firstname', 'lastname', 'company', 'email', 'address1', 'address2'),
 			'rightColumn' => array('country', 'city', 'state', 'zip', 'disclaimer')
 		));
 
