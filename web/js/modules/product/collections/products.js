6b69544179d917b205cd4a851c1fc6dd212e38b3
Valentin Vorobyov <vorobyovvalentin@gmail.com>
2018-08-29 12:20:50 +0300

    Add products pagination

--- web/js/modules/product/collections/products.js
+++ web/js/modules/product/collections/products.js
@@ -14,50 +14,52 @@ define([
         paginator_ui: {
             firstPage: 0,
             currentPage: 0,
-            perPage: 100,
+            perPage: 24,
             totalPages: 10
         },
         server_api: {
             os: 1,
             count: true,
-            limit: function () {
-                return this.perPage;
-            },
-            offset: function () {
-                return this.currentPage * this.perPage;
-            }
+            limit: function() { return this.perPage; },
+            offset: function() { return this.currentPage * this.perPage },
+            key: function(){ return this.key; }
         },
         parse: function (response) {
-            if (this.server_api.count) {
-                this.totalRecords = response.totalRecords;
-            } else {
-                this.totalRecords = response.length;
-            }
-            this.totalPages = Math.floor(this.totalRecords / this.perPage);
-            return this.server_api.count ? response.data : response;
+            this.totalCount = _.has(response, 'totalCount') ? response.totalCount : response.length;
+            this.totalPages = Math.ceil(this.totalCount / this.perPage);
+            return _.has(response, 'data') ? response.data : response;
+
         },
         batch: function (method, data, options) {
-            var checked = this.where({checked: true});
+            var checkedProducts = $('#checkedProducts').val();
+            //var checked = this.where({checked: true});
             var url = $('#website_url').val() + 'api/quote/products/';
-            var ids = _.pluck(checked, 'id').join(',');
+            var ids = '';
+            if(checkedProducts !== '' || typeof checkedProducts !== 'undefined') {
+                ids = checkedProducts;
+            }
 
-            url += 'id/' + ids + '/';
+            //var ids = _.pluck(checked, 'id').join(',');
+            if(ids !== '') {
+                url += 'id/' + ids + '/';
 
-            $.ajax({
-                type: method,
-                url: url,
-                dataType: 'json',
-                beforeSend: showSpinner(),
-                data: JSON.stringify(data)
-            }).done(function () {
-                appView.products.pager();
-                if (typeof options != 'undefined' && (options instanceof Object)) {
-                    if (options.hasOwnProperty('success')) {
-                        options.success();
+                $.ajax({
+                    type: method,
+                    url: url,
+                    dataType: 'json',
+                    beforeSend: showSpinner(),
+                    data: JSON.stringify(data)
+                }).done(function () {
+                    appView.products.pager();
+                    if (typeof options != 'undefined' && (options instanceof Object)) {
+                        if (options.hasOwnProperty('success')) {
+                            options.success();
+                        }
                     }
-                }
-
-            })
+                })
+            } else {
+                showMessage('Please select 1 or more products!', false);
+            }
         }
     });
 
--- web/js/modules/product/views/app.js
+++ web/js/modules/product/views/app.js
@@ -2,20 +2,27 @@ define([
     'underscore',
     'backbone',
     '../collections/products',
-    '../views/product'
-], function (_, Backbone, ProductsCollection, ProductView) {
+    '../views/product',
+    'text!../templates/paginator.html'
+], function (_, Backbone, ProductsCollection, ProductView, PaginatorTmpl) {
 
     var quoteListView = Backbone.View.extend({
         el: $('#products-container'),
         events: {
             'keypress #search': 'searchAction',
             'click .ui-menu-item': 'searchAction',
-            'click .add-products': 'addAction'
+            'click .add-products': 'addAction',
+            'click .paginator a.page': 'paginatorAction'
         },
+        templates: {
+            paginator: _.template(PaginatorTmpl)
+        },
+        checkedProducts: [],
         initialize: function () {
-            $('#product-list-holder').html('<div class="spinner"></div>');
+            $('#products').html('<div class="spinner"></div>');
             this.products = new ProductsCollection();
             this.products.server_api.order = 'p.price DESC';
+            this.products.server_api.count = true;
             this.products.on('reset', this.render, this);
             this.products.fetch();
 
@@ -71,24 +78,95 @@ define([
             }});
         },
         render: function () {
+            var self = this;
+
             this.$('#products').empty();
+            this.$('.products-paginator').empty();
             this.products.each(function (product) {
+                var checked = false;
+
+                if(_.contains(self.checkedProducts, product.get('id')/*.toString()*/)) {
+                    checked = true;
+                    product.set({checked: true});
+                }
                 var view = new ProductView({model: product});
+
+                if(checked) {
+                    $(view.el).addClass('quote-checked');
+                }
                 $(view.render().el).appendTo('#products');
             });
 
             if (this.products.length === 1) {
                 this.renderRelatedFor(this.products.first());
             } else if(this.products.length === 0) {
-                $('#products').html('<p class="nothing">'+$('#product-list-holder').data('emptymsg')+'</p>');
+                $('#products').html('<p class="nothing">'+$('#products').data('emptymsg')+'</p>');
             }
 
             this.$('img.lazy').lazyload({
                 container: this.$('#products'),
                 effect: 'fadeIn'
             });
+            var paginatorData = {
+                collection : 'products'
+            };
+            paginatorData = _.extend(paginatorData, this.products.info());
+            this.$('.products-paginator').html(this.templates.paginator(paginatorData));
+        },
+        paginatorAction:  function(e){
+            var self = this;
+            var products = this.products;
+            products.each(function (product) {
+                var productId = product.get('id');
+               if(product.get('checked')) {
+                   if (!_.isUndefined(self.checkedProducts)) {
+                       self.checkedProducts = _.union(self.checkedProducts, productId);
+                   } else {
+                       self.checkedProducts = [productId];
+                   }
+               } else if(_.contains(self.checkedProducts,productId)) {
+                   var arrayIndex = self.checkedProducts.indexOf(productId);
+                   self.checkedProducts.splice(arrayIndex, 1);
+               }
+            });
+
+            var page = $(e.currentTarget).data('page');
+            var collection = $(e.currentTarget).parent('.paginator').data('collection');
+            if (!collection) return false;
+            if (_.has(this, collection)){
+                collection = this[collection];
+            }
+
+            switch (page) {
+                case 'first':
+                    collection.goTo(collection.firstPage);
+                    break;
+                case 'prev':
+                    if (collection instanceof Backbone.Paginator.requestPager){
+                        collection.requestPreviousPage();
+                    } else {
+                        collection.previousPage();
+                    }
+                    break;
+                case 'next':
+                    if (collection instanceof Backbone.Paginator.requestPager){
+                        collection.requestNextPage();
+                    } else {
+                        collection.nextPage();
+                    }
+                    break;
+                case 'last':
+                    collection.goTo(collection.totalPages);
+                    break;
+                default:
+                    var pageId = parseInt(page);
+                    !_.isNaN(pageId) && collection.goTo(pageId);
+                    break;
+            }
+            return false;
         },
         renderRelatedFor: function (product) {
+            var self = this;
             if (!_.isEmpty(product.get('related'))) {
                 var relateds = new ProductsCollection(),
                     placeholder = $('<img class="mt50px" src="' + $('#website_url').val() + 'system/images/spinner2.gif">');
@@ -98,6 +176,9 @@ define([
                 relateds.on('reset', function (collection) {
                     placeholder.remove();
                     collection.each(function (product) {
+                        if(_.contains(self.checkedProducts,product.get('id'))) {
+                            product.set({checked: true});
+                        }
                         var view = new ProductView({model: product});
                         $(view.render().el).appendTo('#products');
                     });
@@ -109,11 +190,13 @@ define([
         },
         searchAction: function (e, force) {
             if (e.keyCode == 13 || force) {
+                this.products.server_api.count = true;
                 this.products.server_api.key = function () {
                     return e.currentTarget.value;
                 };
-                $('#product-list-holder').html('<div class="spinner"></div>');
-                this.products.pager();
+                $('#products').html('<div class="spinner"></div>');
+                //this.products.pager();
+                this.products.goTo(this.products.firstPage);
                 $(e.target).autocomplete('close');
             }
         }
