10725941c3a292d610012fb761ab6c6e3432dc08
andreybudchenko <andrei@seosamba.com>
2020-08-12 13:52:40 +0300

    add processing pdf during quote sign

--- system/app/Api/Quote/Signature.php
+++ system/app/Api/Quote/Signature.php
@@ -47,6 +47,72 @@ class Api_Quote_Signature extends Api_Service_Abstract
         $quote->setSignature($signature);
         $quote->setQuoteSignedAt(Tools_System_Tools::convertDateFromTimezone('now'));
 
+        $pdfTemplate = $quote->getPdfTemplate();
+        if (!empty($pdfTemplate)) {
+            $pdfTemplate = Application_Model_Mappers_TemplateMapper::getInstance()->find($pdfTemplate);
+            if ($pdfTemplate instanceof Application_Model_Models_Template) {
+                $websiteConfig = Zend_Registry::get('website');
+                $pdfPath = $websiteConfig['path'] . 'plugins' . DIRECTORY_SEPARATOR . 'invoicetopdf' . DIRECTORY_SEPARATOR . 'invoices' . DIRECTORY_SEPARATOR;
+                if (!defined('_MPDF_TEMP_PATH')) {
+                    define('_MPDF_TEMP_PATH', $pdfPath);
+                }
+
+                require_once($websiteConfig['path'] . 'plugins' . DIRECTORY_SEPARATOR . 'invoicetopdf' . DIRECTORY_SEPARATOR . 'system/library/mpdf/mpdf.php');
+                $pageMapper = Application_Model_Mappers_PageMapper::getInstance();
+                $websiteHelper = Zend_Controller_Action_HelperBroker::getStaticHelper('website');
+                $themeData = Zend_Registry::get('theme');
+
+                $parserOptions = array(
+                    'websiteUrl' => $websiteHelper->getUrl(),
+                    'websitePath' => $websiteHelper->getPath(),
+                    'currentTheme' => $websiteHelper->getConfig('currentTheme'),
+                    'themePath' => $themeData['path'],
+                );
+                $page = $pageMapper->findByUrl($quote->getId() . '.html');
+                $page = $page->toArray();
+
+                $parser = new Tools_Content_Parser($pdfTemplate->getContent(), $page, $parserOptions);
+                $content = $parser->parse();
+
+                $pdfFile = new mPDF('utf-8', 'A4');
+                $pdfFile->WriteHTML($content);
+
+                $session = Zend_Controller_Action_HelperBroker::getExistingHelper('session');
+                $session->storeCartSessionConversionKey = $quote->getCartId();
+
+                $pdfFileName = 'Quote_' . md5($quote->getId() . microtime()) . '.pdf';
+
+                $storedData = Tools_LeadDocumentsTools::generateStoredName(array('name' => $pdfFileName));
+
+                $leadsMapper = Leads_Mapper_LeadsMapper::getInstance();
+                $leadModel = $leadsMapper->findByUserId($quote->getUserId());
+                if (!$leadModel instanceof Leads_Model_LeadsModel) {
+                    $userModel = Application_Model_Mappers_UserMapper::getInstance()->find($quote->getUserId());
+                    if ($userModel instanceof Application_Model_Models_User) {
+                        $leadModel = $leadsMapper->findByEmail($userModel->getEmail());
+                    }
+
+                }
+
+                if ($leadModel instanceof Leads_Model_LeadsModel) {
+                    $savePath = Tools_LeadDocumentsTools::getFilePath($storedData['fileStoredName']);
+                    $leadId = $leadModel->getId();
+                    $leadsDocumentsMapper = Leads_Mapper_LeadsDocumentsMapper::getInstance();
+                    $leadsDocumentsModel = new Leads_Model_LeadsDocumentsModel();
+                    $leadsDocumentsModel->setLeadId($leadId);
+                    $leadsDocumentsModel->setFileStoredName($storedData['fileStoredName']);
+                    $leadsDocumentsModel->setFileHash($storedData['fileHash']);
+                    $leadsDocumentsModel->setOriginalFileName($storedData['fileName'] . '.' . $storedData['fileExtension']);
+                    $leadsDocumentsModel->setDisplayFileName($storedData['fileName']);
+                    $leadsDocumentsModel->setUploadedAt(date(Tools_System_Tools::DATE_MYSQL));
+
+                    $leadsDocumentsMapper->save($leadsDocumentsModel);
+                    $pdfFile->Output($savePath, 'F');
+                }
+
+            }
+        }
+
         if ($quote->getPaymentType() === Quote_Models_Model_Quote::PAYMENT_TYPE_ONLY_SIGNATURE) {
             $quote->setStatus(Quote_Models_Model_Quote::STATUS_SOLD);
             $quote->registerObserver(new Quote_Tools_Watchdog(array(
@@ -55,6 +121,7 @@ class Api_Quote_Signature extends Api_Service_Abstract
                 'action' => Tools_System_GarbageCollector::CLEAN_ONUPDATE
             )));
         }
+
         $quoteMapper->save($quote);
 
         $this->_responseHelper->success($translator->translate('Quote has been signed'));
