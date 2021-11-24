0a59805c82c761dba948e2ebe3f284ab0876496b
andreybudchenko <andrei@seosamba.com>
2021-11-24 14:13:36 +0200

    Add quote pdf generation method

--- system/app/Quote/Tools/Tools.php
+++ system/app/Quote/Tools/Tools.php
@@ -920,4 +920,96 @@ class Quote_Tools_Tools {
 
         return array('error' => '0');
     }
+
+    public static function generateQuotePdf($quoteId)
+    {
+        $translator = Zend_Registry::get('Zend_Translate');
+
+        $quoteModel = Quote_Models_Mapper_QuoteMapper::getInstance()->find($quoteId);
+        if (!$quoteModel instanceof Quote_Models_Model_Quote) {
+            return array('error' => '1', 'message' => $translator->translate('Quote not found'));
+        }
+
+        $pdfTemplate = Application_Model_Mappers_TemplateMapper::getInstance()->find($quoteModel->getPdfTemplate());
+        if (!$pdfTemplate instanceof Application_Model_Models_Template) {
+            return array('error' => '1', 'message' => $translator->translate('Quote pdf not found'));
+        }
+
+        $quoteProposalQuote = Models_Mapper_ShoppingConfig::getInstance()->getConfigParam('quoteDownloadLabel');
+
+        if (!empty($quoteProposalQuote)) {
+            $pdfFileName = $quoteProposalQuote.'-' . $quoteModel->getTitle() . '.pdf';
+            $pdfFileNameLocally = $quoteProposalQuote.'-' . $quoteModel->getId() . '.pdf';
+        } else {
+            $pdfFileName = 'Proposal-quote-' . $quoteModel->getTitle() . '.pdf';
+            $pdfFileNameLocally = 'Proposal-quote-' . $quoteModel->getId() . '.pdf';
+        }
+
+        $storedData = Tools_LeadDocumentsTools::generateStoredName(array('name' => $pdfFileName));
+        $storedDataLocally = Quote_Tools_Tools::generateStoredName(array('name' => $pdfFileNameLocally), false);
+
+        $websiteConfig = Zend_Registry::get('website');
+        $pdfPath = $websiteConfig['path'] . 'plugins' . DIRECTORY_SEPARATOR . 'invoicetopdf' . DIRECTORY_SEPARATOR . 'invoices' . DIRECTORY_SEPARATOR;
+        if (!defined('_MPDF_TEMP_PATH')) {
+            define('_MPDF_TEMP_PATH', $pdfPath);
+        }
+
+        require_once($websiteConfig['path'] . 'plugins' . DIRECTORY_SEPARATOR . 'invoicetopdf' . DIRECTORY_SEPARATOR . 'system/library/mpdf/mpdf.php');
+        $pageMapper = Application_Model_Mappers_PageMapper::getInstance();
+        $websiteHelper = Zend_Controller_Action_HelperBroker::getStaticHelper('website');
+        $themeData = Zend_Registry::get('theme');
+
+        $session = Zend_Controller_Action_HelperBroker::getExistingHelper('session');
+        $session->storeCartSessionConversionKey = $quoteModel->getCartId();
+        $registry = Zend_Registry::getInstance();
+        $registry->set('processingAutoQuoteId', $quoteModel->getId());
+
+        $parserOptions = array(
+            'websiteUrl' => $websiteHelper->getUrl(),
+            'websitePath' => $websiteHelper->getPath(),
+            'currentTheme' => $websiteHelper->getConfig('currentTheme'),
+            'themePath' => $themeData['path'],
+        );
+        $page = $pageMapper->findByUrl($quoteModel->getId() . '.html');
+        $page = $page->toArray();
+
+        $parser = new Tools_Content_Parser($pdfTemplate->getContent(), $page, $parserOptions);
+        $content = $parser->parse();
+
+        $pdfFile = new mPDF('utf-8', 'A4');
+        $pdfFile->WriteHTML($content);
+
+        $quoteSaveLocallyPath = Quote_Tools_Tools::getFilePath($storedDataLocally['fileStoredName']);
+        $pdfFile->Output($quoteSaveLocallyPath, 'F');
+
+        $leadsMapper = Leads_Mapper_LeadsMapper::getInstance();
+        $leadModel = $leadsMapper->findByUserId($quoteModel->getUserId());
+        if (!$leadModel instanceof Leads_Model_LeadsModel) {
+            $userModel = Application_Model_Mappers_UserMapper::getInstance()->find($quoteModel->getUserId());
+            if ($userModel instanceof Application_Model_Models_User) {
+                $leadModel = $leadsMapper->findByEmail($userModel->getEmail());
+            }
+        }
+
+        if ($leadModel instanceof Leads_Model_LeadsModel) {
+            $savePath = Tools_LeadDocumentsTools::getFilePath($storedData['fileStoredName']);
+
+            $leadId = $leadModel->getId();
+            $leadsDocumentsMapper = Leads_Mapper_LeadsDocumentsMapper::getInstance();
+            $leadsDocumentsModel = new Leads_Model_LeadsDocumentsModel();
+            $leadsDocumentsModel->setLeadId($leadId);
+            $leadsDocumentsModel->setFileStoredName($storedData['fileStoredName']);
+            $leadsDocumentsModel->setFileHash($storedData['fileHash']);
+            $leadsDocumentsModel->setOriginalFileName($storedData['fileName'] . '.' . $storedData['fileExtension']);
+            $leadsDocumentsModel->setDisplayFileName($storedData['fileName']);
+            $leadsDocumentsModel->setUploadedAt(date(Tools_System_Tools::DATE_MYSQL));
+
+            $leadsDocumentsMapper->save($leadsDocumentsModel);
+            $pdfFile->Output($savePath, 'F');
+        }
+
+        return array('error' => '0');
+
+    }
+
 }
